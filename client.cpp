81e6fc4dc286dc9cfd90e200cc62f2eb3575da22
theosmuller <theosmuller@gmail.com>
2022-04-02 14:43:46 -0300

    A principio ta pronto o client, falta implementar o server e eventualmente tirar as constantes desse arquivo e tacar pra um em comum

--- client.cpp
+++ client.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
 #include "packet.cpp"
 
 #define PORT 4000
@@ -15,6 +16,9 @@
 #define QUIT 1
 #define SEND 2
 #define FOLLOW 3
+#define NOTIFICATION 4
+#define SERVERMSG 5
+#define LOGUSER 6
 #define TRUE 1
 
 int sockfd;
@@ -24,47 +28,52 @@ int main(int argc, char *argv[])
 {
 	pthread_t thr_client_input, thr_client_display;
 	
-    int n;
+    int n, port;
 	unsigned int length;
+	char handle[20];
 	struct sockaddr_in serv_addr, from;
 	struct hostent *server;
 	
 	char buffer[256];
-	if (argc < 2) {
-		fprintf(stderr, "usage %s hostname\n", argv[0]);
+	if (argc < 4) {
+		fprintf(stderr, "usage ./app_cliente <@profile> <server_address> <port> \n");
 		exit(0);
 
 	}
 	
-	server = gethostbyname(argv[1]);
+	server = gethostbyname(argv[2]);
 	if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
-    }	
-	
+    }
+
+	port = atoi(argv[3]);
+
+	strcpy(handle, argv[1]);	
+	validateuserhandle(handle);
+	printf("User handle: %s , Port: %d\nUse SEND to send a message to all followers.\nUse FOLLOW <@handle> to follow another user.",handle, port);
+
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 		printf("ERROR opening socket");
 	
 	serv_addr.sin_family = AF_INET;     
-	serv_addr.sin_port = htons(PORT);    
+	serv_addr.sin_port = htons(port);    
 	serv_addr.sin_addr = *((struct in_addr *)server->h_addr);
 	bzero(&(serv_addr.sin_zero), 8);  
 
-	printf("Enter the message: ");
-	bzero(buffer, 256);
-	fgets(buffer, 256, stdin);
+	if((connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0)){
+		printf("Socket connection error\n");
+	}
 
-	n = sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *) &serv_addr, sizeof(struct sockaddr_in));
-	if (n < 0) 
-		printf("ERROR sendto");
-	
-	length = sizeof(struct sockaddr_in);
-	n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *) &from, &length);
-	if (n < 0)
-		printf("ERROR recvfrom");
+	sendhandletoserver(handle);
 
-	printf("Got an ack: %s\n", buffer);
+
+//Começa uma thread para receber mensagens e outra para enviar
+	pthread_create(&thr_client_display, NULL, receivemessage, &sockfd);
+	pthread_create(&thr_client_input, NULL, sendmessage, &sockfd);
 	
+	pthread_join(thr_client_display, NULL);
+	pthread_join(thr_client_input, NULL);
 	close(sockfd);
 	return 0;
 }
@@ -76,7 +85,7 @@ void *sendmessage(void *arg){
 	int n, flag, action, sockfd = *(int *) arg; 
 	char buffer[BUFFER_SIZE];
 
-	while(1)
+	while(TRUE)
 	{
 		//clears buffer
 		bzero(buffer, BUFFER_SIZE);
@@ -106,6 +115,31 @@ void *sendmessage(void *arg){
 	}
 }
 
+void *receivemessage(void *arg){
+	int sockfd = *(int *) arg;
+	packet msg;
+
+	while(TRUE){
+		recvpacket(sockfd, &msg);
+		switch(msg.type){
+			case FOLLOW:
+				printf("%s\n", msg._payload);
+				break;
+			case NOTIFICATION:
+				printf("%s\n", msg._payload);
+				break;
+			case QUIT:
+				printf ("Limite de sessões simultâneas excedido.");
+				close (sockfd);
+				exit(1);
+				break;
+			default:
+				printf ("Unknown message error");
+				break;
+		}
+		free(msg._payload);
+	}
+}
 
 int getaction(char* buffer){
 	if(!strncmp(buffer, "SEND ", 5))
@@ -134,4 +168,20 @@ int getcurrenttime(){
 	int min = (*structtm).tm_min;
 
 	return hr*100+min;
+}
+
+void validateuserhandle(char *handle){
+	//Testa o comprimento do username
+	if (!(strlen(handle) <= 20 && strlen(handle)>=4)){
+		printf("Username must have between 4 and 20 characters.");
+		exit(0);
+	}
+	//Testa se o primeiro caractere é @
+	if((handle[0]!='@')){
+		printf("Handle must start with @\nExample: @TestUser");
+	}
+}
+
+void sendhandletoserver(char *handle){
+	sendpacket(sockfd, LOGUSER, ++seqncnt, strlen(handle)+1, getcurrenttime(), handle);
 }
